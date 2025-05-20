54437e0dff2b2403d7083ab502b02d478cf2f8f9
Angus MacMinn <angus13@hotmail.ca>
2025-05-20 14:37:25 -0700

    re-installed dependencies

---
+++ skye-interactive/lib/apolloClient.ts
@@ -0,0 +1,9 @@
+// lib/apolloClient.ts
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_ENDPOINT,
+  cache: new InMemoryCache(),
+});
+
+export default client;
