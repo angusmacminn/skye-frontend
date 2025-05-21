c1ac6e268171de9706cf9b4befa1833743fad3f0
Angus MacMinn <angus13@hotmail.ca>
2025-05-20 17:21:09 -0700

    added some global colours and styles

--- skye-interactive/tailwind.config.ts
+++ skye-interactive/tailwind.config.ts
@@ -6,7 +6,23 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'skye-primary-red': '#EF4444',
+        'skye-primary-red-light': '#F87171',
+        'skye-primary-blue': '#5A4FF2',
+        'skye-primary-blue-light': '#818CF8',
+        'skye-black': '#1C1414', 
+        'skye-gray': '#424141',
+        'skye-white': '#F5F5F5',
+      },
+      fontFamily: {
+        'radio-grotesk': ['Radio Grotesk', 'sans-serif'],
+      },
+      spacing: {
+        
+      }
+    },
   },
   plugins: [],
 }
