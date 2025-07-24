94666cee1b8ffe2ec20b912a4bce5eaa3a120006
Angus MacMinn <angus13@hotmail.ca>
2025-07-24 11:12:28 -0700

    actually added the component lol

--- skye-interactive-v2/src/app/layout.tsx
+++ skye-interactive-v2/src/app/layout.tsx
@@ -39,6 +39,7 @@ export default function RootLayout({
         <meta name="author" content="Skye Interactive" />
       </head>
       <body className="antialiased">
+        <Analytics />
         <GsapProvider />
         <SkipLink />
         <LenisProvider />
